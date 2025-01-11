9a8a297f12e101d5f1b6bd0fd8a6353784904a6f
ajinkya-tambe <ajinkyatambe32@gmail.com>
2025-01-11 05:40:54 +0530

    Added HERO section, SearchBar, Startup Cards. Sanity is setup and ready to use

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,81 @@
-export default function Home() {
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Ajinkya Tambe" },
+      _id: 1,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      image: "https://i.ibb.co/BCQC36G/robots.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+    {
+      _createdAt: new Date(),
+      views: 95,
+      author: { _id: 2, name: "Anil Gupta" },
+      _id: 2,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      image: "https://i.ibb.co/BCQC36G/robots.jpg",
+      category: "Games",
+      title: "Gaming is Future",
+    },
+    {
+      _createdAt: new Date(),
+      views: 755,
+      author: { _id: 3, name: "Amol Seni" },
+      _id: 3,
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
+      image: "https://i.ibb.co/BCQC36G/robots.jpg",
+      category: "Technology",
+      title: "AI vs Human",
+    },
+  ];
+
   return (
     <>
-      <h1 className="text-2xl">HOME</h1>
+      {/* Hero Section */}
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch your startup,
+          <br /> Connect with Entrepreneurs
+        </h1>
+
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      {/* Startup Cards */}
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartCardType) => (
+              <StartupCard key={post?._id} post={post} />
+            ))
+          ) : (
+            <p className="no-result">No startup found</p>
+          )}
+        </ul>
+      </section>
     </>
   );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    dangerouslyAllowSVG: true,
     domains: ['lh3.googleusercontent.com'], // Add Google profile image domain here
+    remotePatterns: [
+      {
+        // protocol: 'https:',
+        hostname: '*',
+      }
+    ]
   },
 };
 
