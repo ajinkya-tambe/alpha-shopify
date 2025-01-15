636659d988a05a3eb86661ac639c895f42744a14
ajinkya-tambe <ajinkyatambe32@gmail.com>
2025-01-15 18:50:47 +0530

    Profile pic added in StartupCard

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -41,7 +41,8 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
         </div>
         <Link href={`/user/${author?._id}`}>
           <Image
-            src="https://placehold.co/48x48"
+            // src="https://placehold.co/48x48"
+            src={author?.image || "https://placehold.co/48x48"}
             alt="placeholder"
             width={48}
             height={48}
