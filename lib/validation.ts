b4f8ae27bea01245502070d48790fa70dcba65e0
ajinkya-tambe <ajinkyatambe32@gmail.com>
2025-01-18 12:45:57 +0530

    lil changes in form validation

--- lib/validation.ts
+++ lib/validation.ts
@@ -1,21 +1,23 @@
-import {z} from 'zod';
-
-
+import { z } from "zod";
 
 export const formSchema = z.object({
-    title: z.string().min(3).max(100),
-    description: z.string().min(20).max(1000),
-    category: z.string().min(3).max(50),
-    link: z.string().url().refine(async (url) => {
-        try{
-            const res = await fetch(url, {method: 'HEAD'});
-            const contentType = res.headers.get("content-type");
+  title: z.string().min(3).max(100),
+  description: z.string().min(20).max(1000),
+  category: z.string().min(3).max(50),
+  link: z
+    .string()
+    .url()
+    .refine(async (url) => {
+      try {
+        const res = await fetch(url, { method: "HEAD" });
+        const contentType = res.headers.get("content-type");
 
-            return contentType ?. startsWith('image/');
-        }
-        catch{
-            return false;
-        }
-    }),
-    pitch: z.string().min(10),
-})
+        // Loosen the validation to accept more types of links.
+        return contentType ? contentType.includes("image") : true;
+      } catch {
+        // Allow URLs even if the `HEAD` request fails, as long as they are valid URLs.
+        return true;
+      }
+    }, "The provided URL must point to an image."),
+  pitch: z.string().min(10),
+});
